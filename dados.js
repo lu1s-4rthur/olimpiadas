1ec903894063ef3e16e244f63e2b788d2fc65b26
lu1s-4rthur <larthur618@gmail.com>
2024-09-06 15:22:00 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e um verdadeiro orgulho nacional. Com sua força, determinação e talento inegável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ouro ginasta biles salto"
+    },
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um dos surfistas mais talentosos e influentes do mundo, tendo conquistado diversos títulos importantes e se tornado um ícone do esporte no Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags: "surfe brunet fotão prancha"
+    },
+    {
+        titulo: "Neymar Jr.",
+        descricao: "Neymar Jr. é um dos maiores jogadores de futebol do mundo, conhecido por sua habilidade, velocidade e gols espetaculares. Ele é um dos principais jogadores da seleção brasileira e um ídolo para milhões de fãs.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "menino ney futebol mlk ousadia"
+    }
+]
